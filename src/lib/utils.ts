e5668bbb5f574a44a8768f3009e61e922a5ef6e4
Sandeep <agencysandesigns@gmail.com>
2025-09-25 12:18:55 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import nextConfig from "../../../next.config"
+import nextConfig from "../../next.config"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
