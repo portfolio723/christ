436a7925edbcceb4c9d3ffbc5e118b13295c9ce0
Sandeep <agencysandesigns@gmail.com>
2025-09-25 09:29:05 +0000

    https://lh3.googleusercontent.com/gg-dl/AJfQ9KRKE84jH6UYr85Ua2p9HLlg85wz

--- next.config.ts
+++ next.config.ts
@@ -38,6 +38,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
