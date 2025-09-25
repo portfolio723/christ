dafab38a8958928454230f4f660cf403ce63b5bb
Sandeep <agencysandesigns@gmail.com>
2025-09-25 12:17:52 +0000

    now video as background for the many pages are not appearing in both loc

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import nextConfig from "../../../next.config"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const getAssetPath = (path: string) => {
-  return process.env.NODE_ENV === 'production' ? `/christ${path}` : path;
+  if (process.env.NODE_ENV === 'production') {
+    // Ensure assetPrefix ends with a slash and path doesn't start with one.
+    const prefix = nextConfig.assetPrefix?.endsWith('/') ? nextConfig.assetPrefix : `${nextConfig.assetPrefix}/`;
+    const formattedPath = path.startsWith('/') ? path.substring(1) : path;
+    return `${prefix}${formattedPath}`;
+  }
+  return path;
 };
