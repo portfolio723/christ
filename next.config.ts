88acee02be8da1667474fdfbaa086e5fd1753df4
Sandeep <agencysandesigns@gmail.com>
2025-09-26 06:28:23 +0000

    Update next.config.ts Add basePath configuration for GitHub Pages subdir

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/christ',
   typescript: {
     ignoreBuildErrors: true,
   },
