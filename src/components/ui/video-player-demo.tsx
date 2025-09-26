cbf9c9a3739af4210ddef4c49b12936d79fc8f43
Sandeep <agencysandesigns@gmail.com>
2025-09-26 06:06:16 +0000

    replace the video in this src with the rare.mp4 through the out and publ

--- src/components/ui/video-player-demo.tsx
+++ src/components/ui/video-player-demo.tsx
@@ -2,7 +2,7 @@
 import VideoPlayer from "@/components/ui/video-player"
 
 const VideoPlayerDemo = () => {
-    return <VideoPlayer src="https://videos.pexels.com/video-files/30333849/13003128_2560_1440_25fps.mp4"/>
+    return <VideoPlayer src="/rare.mp4"/>
 }
 
 export { VideoPlayerDemo }
