b702aaa7300ede6c43c849582e947774423b46aa
Sandeep <agencysandesigns@gmail.com>
2025-09-30 18:35:10 +0000

    For github pages live, the background hero section videos, images of var

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 
 import type {NextConfig} from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  assetPrefix: isProd ? '/christ/' : undefined,
+  assetPrefix: '/christ/',
   typescript: {
     ignoreBuildErrors: true,
   },
