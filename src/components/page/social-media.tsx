96781f2e1db9ab72cf11bd51c4f0877376fc82e1
Sandeep <agencysandesigns@gmail.com>
2025-10-01 13:12:05 +0000

    replace the svg with the symbol/icon : @ (_for element svg_)

--- src/components/page/social-media.tsx
+++ src/components/page/social-media.tsx
@@ -9,21 +9,7 @@ import {
 import { Card } from '@/components/ui/card';
 
 const ThreadsIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
-  >
-    <path d="M12 8.5A3.5 3.5 0 0 0 8.5 12h7A3.5 3.5 0 0 0 12 8.5Z" />
-    <path d="M12 15.5a3.5 3.5 0 0 1 3.5-3.5h-7a3.5 3.5 0 0 1 3.5 3.5Z" />
-  </svg>
+  <span {...props} style={{ fontSize: '24px', fontWeight: 'bold' }}>@</span>
 );
 
 
