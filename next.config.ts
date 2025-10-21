e69149c69a9cfe9c81f29d30bfd9cbf697bc193e
Sandeep <agencysandesigns@gmail.com>
2025-10-21 13:44:09 +0000

    Image Optimization You’ve disabled image optimization in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,44 +11,6 @@ const nextConfig: NextConfig = {
   // Disable image optimization (required for static export)
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'miro.medium.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
   },
   
   // Add trailing slashes for better compatibility with some static hosts.
