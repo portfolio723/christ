977d2bbb5bfcf3e0ca321a7b64fb7fbc26618041
Sandeep <agencysandesigns@gmail.com>
2025-10-22 05:32:34 +0000

    produce a static site with the proper index.html, 404.html, and CNAME at

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
