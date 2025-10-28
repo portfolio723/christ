0bd796821b423004470bc835356aa8ad03c900f8
Sandeep <agencysandesigns@gmail.com>
2025-10-28 06:26:15 +0000

    yes

---
+++ src/app/sitemap.ts
@@ -0,0 +1,39 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://godsspiritsays.com';
+
+  const pages = [
+    '/',
+    '/about',
+    '/videos',
+    '/videos/breaking-is-not-the-end',
+    '/videos/bubbling-love',
+    '/videos/circle-of-joy',
+    '/videos/fasting',
+    '/videos/feeling-nearness',
+    '/videos/gods-habituality',
+    '/videos/his-love',
+    '/blog',
+    '/blog/beautifully-shattered',
+    '/blog/fish-without-water',
+    '/blog/forgiveness',
+    '/blog/good-friday',
+    '/blog/he-cares',
+    '/blog/marriage',
+    '/blog/pray',
+    '/blog/resurrection-embrace',
+    '/blog/sweetness-bleeds',
+    '/blog/the-beautiful-breaking',
+    '/blog/the-prayer-room',
+    '/testimonials',
+    '/contact',
+  ];
+
+  return pages.map((page) => ({
+    url: `${baseUrl}${page}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: page === '/' ? 1 : 0.8,
+  }));
+}
