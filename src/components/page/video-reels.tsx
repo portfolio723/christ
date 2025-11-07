5c841b456f2b526e8a24b20100147f528617e1c1
Sandeep <agencysandesigns@gmail.com>
2025-11-07 06:28:30 +0000

    reduce the top and bottom padding of the image div (_for element img_)

--- src/components/page/video-reels.tsx
+++ src/components/page/video-reels.tsx
@@ -143,7 +143,7 @@ export function VideoReels() {
           </div>
 
           <div className="mt-16 grid grid-cols-1 items-center justify-items-center bg-card/50 backdrop-blur-sm px-4 rounded-xl border border-primary/20">
-            <div className="text-white/90 text-lg md:text-xl font-crimson-pro space-y-4 text-left max-w-3xl pt-4">
+            <div className="text-white/90 text-lg md:text-xl font-crimson-pro space-y-4 text-left max-w-3xl pt-8">
               <p>The divine dance of love Triumph💛🎺</p>
               <p>Embracing the unique love of the 𝗧𝗥𝗜𝗡𝗜𝗧𝗬 in our story ✨🤍🔥</p>
               <p>𝑭𝒂𝒕𝒉𝒆𝒓: He loves you... for He did not withhold His only Son. 👑❤️‍🔥</p>
